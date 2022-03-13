0e8c74caf9c4e1483ab6adf66be2a1dbc238779d
Alexander Sabanov <alexsab@mail.ru>
2022-03-13 22:30:56 +0400

    обновил для закладок

--- instagram-save-post-links.js
+++ instagram-save-post-links.js
@@ -15,27 +15,28 @@ javascript:
     };
 
     function css(element, style) {
-        for (const property in style)
+        for (const property in style) {
             element.style[property] = style[property];
-    }
+        }
+    };
 
     let posts = [],
         btn = document.createElement("a");
 
-    btn.style.cssText += "position: fixed; background: red; top: 0; left: 0; padding: 10px; color: white;"
-    btn.href = "javascript:void(0);"
+    btn.style.cssText += "position: fixed; background: red; top: 0; left: 0; padding: 10px; color: white;";
+    btn.href = "javascript:void(0);";
     document.body.appendChild(btn);
     btn.addEventListener("click", ()=>{
-        copyToClipboard(posts.join("\n"))
-    })
+        copyToClipboard(posts.join("\n"));
+    });
 
     setInterval(() => {
         document.querySelectorAll('[href^="/p/"]').forEach((p) => {
             if (!posts.includes(p.href)) {
-                posts.push(p.href)
+                posts.push(p.href);
                 btn.innerText = "Скопировать ссылки постов ("+posts.length+")";
             }
         })
-    }, 100)
+    }, 100);
 
 })()
